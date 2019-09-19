e4fa7038e0d68b475d32477e9f2c3818a185c5bc
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2019-09-18 21:55:23 -0300

    removed openfeign rest client and implements rest template client

--- transportadora/src/main/java/br/com/raptorslog/TransportadoraApplication.java
+++ transportadora/src/main/java/br/com/raptorslog/TransportadoraApplication.java
@@ -1,13 +1,12 @@
 package br.com.raptorslog;
 
-import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TransportadoraApplication {
 
 	public static void main(String[] args) {
@@ -15,7 +14,12 @@ public class TransportadoraApplication {
 	}
 
 	@Bean
-	public io.opentracing.Tracer tracer() {
-		return Configuration.fromEnv().getTracer();
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
 	}
+
+//	@Bean
+//	public io.opentracing.Tracer tracer() {
+//		return Configuration.fromEnv().getTracer();
+//	}
 }
---
+++ transportadora/src/main/java/br/com/raptorslog/repository/EntregadorMGImpl.java
@@ -0,0 +1,27 @@
+package br.com.raptorslog.repository;
+
+import br.com.raptorslog.model.Entrega;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+@Repository
+public class EntregadorMGImpl implements EntregadorMG {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${client.entregador-mg.url}")
+    private String remoteURL;
+
+    @Autowired
+    public EntregadorMGImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public ResponseEntity send(Entrega entrega) {
+        return restTemplate.postForEntity(remoteURL.concat("/entregas"), entrega, String.class);
+    }
+}
